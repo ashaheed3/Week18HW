6a8450b4f2c5c1d1515ef8a5deda036d2e9981af
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-07-11 15:52:21 -0400

    Added db.js

--- public/db.js
+++ public/db.js
@@ -1,10 +1,10 @@
 let db;
-// create a new db request for a "budget" database.
+
 const request = window.indexedDB.open("budget", 1);
 
 
 request.onupgradeneeded = function(event) {
-  // create object store called "pending" and set autoIncrement to true
+  
 
  db = event.target.result;
 
@@ -22,19 +22,16 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  // log error here
+  /
 };
 
 function saveRecord(record) {
-  // create a transaction on the pending db with readwrite access
-  // access your pending object store
-  // add record to your store with add method.
- 
+  
     db = request.result;
     transaction = db.transaction(["pending"], "readwrite");
     pendingStore = transaction.objectStore("pending");
         
-  // Adds data to our objectStore
+ 
   pendingStore.add(record);
   
 }
@@ -44,24 +41,24 @@ function checkDatabase() {
   // access your pending object store
   // get all records from store and set to a variable
 
-//   getAll.onsuccess = function() {
-//     if (getAll.result.length > 0) {
-//       fetch("/api/transaction/bulk", {
-//         method: "POST",
-//         body: JSON.stringify(getAll.result),
-//         headers: {
-//           Accept: "application/json, text/plain, */*",
-//           "Content-Type": "application/json"
-//         }
-//       })
-//       .then(response => response.json())
-//       .then(() => {
-//           // if successful, open a transaction on your pending db
-//           // access your pending object store
-//           // clear all items in your store
-//       });
-//     }
-//   };
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+          // if successful, open a transaction on your pending db
+          // access your pending object store
+          // clear all items in your store
+      });
+    }
+  };
 }
 
 // listen for app coming back online
